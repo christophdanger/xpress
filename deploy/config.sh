53aee5e3d9029440865aa836d7f7cce70e024d34
CM Smith <cmsmitty84@gmail.com>
2025-07-04 18:09:36 -0400

    fix: revert Traefik domain and email to example.com in config.sh

--- deploy/config.sh
+++ deploy/config.sh
@@ -7,8 +7,8 @@ export FRAPPE_DOCKER_DIR="${WORKDIR}/frappe_docker"
 export FRAPPE_DOCKER_REPO="https://github.com/christophdanger/devburner-frappe_docker.git"
 
 # --- Traefik Configuration ---
-export TRAEFIK_DOMAIN="traefik-erpnext.devburner.io"
-export TRAEFIK_EMAIL="cmsmitty84+traefik-mmp@gmail.com"
+export TRAEFIK_DOMAIN="traefik.example.com"
+export TRAEFIK_EMAIL="user@example.com"
 export TRAEFIK_PASSWORD="##################"
 
 # --- MariaDB Configuration ---
@@ -16,4 +16,4 @@ export MARIADB_PASSWORD="##################"
 
 # --- Bench & Site Configuration ---
 export BENCH1_NAME="erpnext-one"
-export BENCH1_SITES=("erp.devburner.io")
+export BENCH1_SITES=("erp.example.com") # List of sites for bench1
